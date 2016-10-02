87074c6135b91bb8c3c7895c9ac3603ceac9a15f
Vit Listik <tivvit@seznam.cz>
2016-10-02 16:35:36 +0200

    just to be safe

--- server/main.py
+++ server/main.py
@@ -27,65 +27,69 @@ def net_data(data, res, global_coef):
 
 @app.route("/")
 def hello():
-    nelat = float(request.args.get('nelat'))
-    nelng = float(request.args.get('nelng'))
-    swlng = float(request.args.get('swlng'))
-    swlat = float(request.args.get('swlat'))
-    zoom = request.args.get('zoom')
-    cell_coef = float(request.args.get('cell'))
-    local_coef = float(request.args.get('local'))
-    pollution_coef = float(request.args.get('pollution'))
+    try:
+        nelat = float(request.args.get('nelat'))
+        nelng = float(request.args.get('nelng'))
+        swlng = float(request.args.get('swlng'))
+        swlat = float(request.args.get('swlat'))
+        zoom = request.args.get('zoom')
+        cell_coef = float(request.args.get('cell'))
+        local_coef = float(request.args.get('local'))
+        pollution_coef = float(request.args.get('pollution'))
 
-    conn = _get_engine()
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    cur.execute(
-        """SELECT ST_X(n.pos::geometry) as lng, ST_Y(n.pos::geometry) as lat,
-          n.down, n.up, n.ping, n.tech, n.pos_id, p.value as pollution from a_netmetr as n
-          JOIN air_polution_data as p ON p.grid_id = n.pos_id
-          WHERE ST_Contains(ST_MakeEnvelope(%s, %s, %s, %s, 4326),
-          CAST(n.pos as geometry))
-          """, (swlng, swlat, nelng, nelat))
-    rows = cur.fetchall()
-    data = [dict(i) for i in rows]
-    # None to 0.5
-    data = [{k: v if v else (0.5 if k in {"down", "up", "ping"} else None)
-             for k, v in i.items()} for i in data]
+        conn = _get_engine()
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute(
+            """SELECT ST_X(n.pos::geometry) as lng, ST_Y(n.pos::geometry) as lat,
+              n.down, n.up, n.ping, n.tech, n.pos_id, p.value as pollution from a_netmetr as n
+              JOIN air_polution_data as p ON p.grid_id = n.pos_id
+              WHERE ST_Contains(ST_MakeEnvelope(%s, %s, %s, %s, 4326),
+              CAST(n.pos as geometry))
+              """, (swlng, swlat, nelng, nelat))
+        rows = cur.fetchall()
+        data = [dict(i) for i in rows]
+        # None to 0.5
+        data = [{k: v if v else (0.5 if k in {"down", "up", "ping"} else None)
+                 for k, v in i.items()} for i in data]
 
-    local = [i for i in data if not i["tech"] or i["tech"] == "local"]
-    cell = [i for i in data if not i["tech"] or i["tech"] == "cell"]
+        local = [i for i in data if not i["tech"] or i["tech"] == "local"]
+        cell = [i for i in data if not i["tech"] or i["tech"] == "cell"]
 
-    results = {
-        i["pos_id"]: {
-            "lat": i["lat"],
-            "lng": i["lng"],
-            "values": [],
-        } for i in local
-        }
-
-    for i in cell:
-        if i["pos_id"] not in results:
-            results[i["pos_id"]] = {
+        results = {
+            i["pos_id"]: {
                 "lat": i["lat"],
                 "lng": i["lng"],
                 "values": [],
+            } for i in local
             }
 
-    for i in data:
-        results[i["pos_id"]]["values"].append(i["pollution"] * -pollution_coef)
+        for i in cell:
+            if i["pos_id"] not in results:
+                results[i["pos_id"]] = {
+                    "lat": i["lat"],
+                    "lng": i["lng"],
+                    "values": [],
+                }
 
-    net_data(local, results, local_coef)
-    net_data(cell, results, cell_coef)
+        for i in data:
+            if i["pollution"]:
+                results[i["pos_id"]]["values"].append(i["pollution"] * -pollution_coef)
 
-    ret = [{
-               "lat": i["lat"],
-               "lng": i["lng"],
-               "val": sum(i["values"]) / len(i["values"])
-           } for i in results.values()]
+        net_data(local, results, local_coef)
+        net_data(cell, results, cell_coef)
 
-    resp = flask.Response(json.dumps(ret))
-    resp.headers['Content-type'] = 'text/json'
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
+        ret = [{
+                   "lat": i["lat"],
+                   "lng": i["lng"],
+                   "val": sum(i["values"]) / len(i["values"])
+               } for i in results.values()]
+
+        resp = flask.Response(json.dumps(ret))
+        resp.headers['Content-type'] = 'text/json'
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp
+    except:
+        return json.dumps([])
 
 
 if __name__ == "__main__":
